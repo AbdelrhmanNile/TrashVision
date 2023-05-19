98c193d12f839148560b0e83d41c084539e0c7a2
AhmedAbozaid94 <ae826252@gmail.com>
2023-05-19 21:49:12 +0300

    Update data.py

--- software/database/data.py
+++ software/database/data.py
@@ -3,9 +3,8 @@ import time
 import cv2
 import os
 from dotenv import load_dotenv
-
 load_dotenv("./.env")
-
+import numpy as np
 
 class DataBase:
     def __init__(self):
@@ -13,27 +12,27 @@ class DataBase:
         self.db = self.client[os.getenv("db_name")]
         self.collection = self.db[os.getenv("collection_name")]
 
-        self.db_count = self.get_len()
 
     def insert_image(self, time_stamp, img, label=""):
 
         is_success, im_buf_arr = cv2.imencode(".jpg", img)
         binary_data = im_buf_arr.tobytes()
 
-        self.collection.insert_one(
-            {
-                "_id": self.db_count + 1,
-                "time": time_stamp,
-                "img": binary_data,
-                "label": label,
-            }
-        )
-
-        self.db_count += 1
+        self.collection.insert_one({"_id": self.get_len()+1,
+                                    "time": time_stamp,
+                                    "img": binary_data,
+                                    "label": label})
         return True
 
+    #def data_generator(self):
     def data_generator(self):
-        raise NotImplementedError("This function is not implemented yet")
+        cursor = self.collection.find()
+        for document in cursor:
+            image_bytes = document["img"]
+            image_array = np.frombuffer(image_bytes, dtype=np.uint8)
+            image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+            label = document["label"]
+            yield image, label
 
     def get_len(self):
         data = self.collection.find({})
@@ -42,3 +41,4 @@ class DataBase:
     def remove_all(self):
         self.collection.delete_many({})
         return True
+
